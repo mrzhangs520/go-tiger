7aa8a68a18a3477db4b930a34e251908ac0ab6d0
狮子 <mrzhangs@yeah.net>
2022-09-14 15:01:52 +0800

    广泛用的go包🍎

---
+++ dbManager/dbManager.go
@@ -0,0 +1,56 @@
+package dbManager
+
+import (
+	"fmt"
+	"github.com/mrzhangs520/go-tiger/config"
+	"github.com/mrzhangs520/go-tiger/core"
+	"github.com/mrzhangs520/go-tiger/dError"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"time"
+)
+
+var db *gorm.DB
+
+func init() {
+	core.Start()
+
+	mysqlConfig := config.GetInstance().Section("mysql")
+	username := mysqlConfig.Key("username").Value()
+	password := mysqlConfig.Key("password").Value()
+	host := mysqlConfig.Key("host").Value()
+	port := mysqlConfig.Key("port").Value()
+	dbname := mysqlConfig.Key("dbname").Value()
+	timeout := mysqlConfig.Key("timeout").Value()
+
+	//拼接下dsn参数, dsn格式可以参考上面的语法，这里使用Sprintf动态拼接dsn参数，因为一般数据库连接参数，我们都是保存在配置文件里面，需要从配置文件加载参数，然后拼接dsn。
+	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&loc=Local&timeout=%s", username, password, host, port, dbname, timeout)
+
+	var err error
+
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if nil != err {
+		panic(dError.NewError("dbManager.init.gorm.Open", err))
+	}
+	// 控制数据库连接池
+	sqlDB, err := db.DB()
+
+	if nil != err {
+		panic(dError.NewError("dbManager.init.db.DB", err))
+	}
+
+	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(100)
+
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(time.Hour)
+}
+
+func GetInstance() *gorm.DB {
+	return db
+}
---
+++ dbManager/dbManager_test.go
@@ -0,0 +1,50 @@
+package dbManager
+
+import (
+	"github.com/davecgh/go-spew/spew"
+	"gorm.io/gorm"
+	"testing"
+	"time"
+)
+
+type BaseModel struct {
+	CreatedTime string `gorm:"column:create_time" json:"create_time"`
+	UpdatedTime string `gorm:"column:update_time" json:"update_time"`
+}
+
+func (b *BaseModel) BeforeCreate(*gorm.DB) error {
+	b.CreatedTime = time.Now().Format("2006-01-02 15:16:05")
+	b.UpdatedTime = b.CreatedTime
+	return nil
+}
+
+func (b BaseModel) BeforeUpdate(*gorm.DB) error {
+	b.UpdatedTime = time.Now().Format("2006-01-02 15:16:05")
+	return nil
+}
+
+type AdminModel struct {
+	BaseModel
+	Id        uint   `gorm:"primaryKey" json:"id"`
+	AdminName string `json:"admin_name"`
+	Password  string `json:"-"`
+	Mobile    string `json:"phone"`
+}
+
+func (a *AdminModel) TableName() string {
+	return "v1_admin"
+}
+
+func TestCreate(t *testing.T) {
+	adminInfo := new(AdminModel)
+
+	GetInstance().Debug().Create(adminInfo)
+	spew.Dump(adminInfo.Id)
+}
+
+func TestGet(t *testing.T) {
+	adminInfo := new(AdminModel)
+
+	GetInstance().Debug().Where(10).First(adminInfo)
+	spew.Dump(adminInfo.CreatedTime)
+}
