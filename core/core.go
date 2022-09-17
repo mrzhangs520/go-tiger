80fa3fda807d6595db397be952bcc4c07f61c071
mrzhangs520 <mrzhangs@yeah.net>
2022-09-17 16:56:30 +0800

    更换生成日志目录

--- core/core.go
+++ core/core.go
@@ -8,7 +8,7 @@ import (
 
 func Start() {
 	// 创建temp文件夹
-	_ = os.Mkdir(fmt.Sprintf("%s/temp", AppPath), 0777)
+	_ = os.Mkdir(fmt.Sprintf("%s/log", AppPath), 0777)
 
 	fmt.Printf("当前时间：%s\n", time.Now().Format("2006-01-02 15:04:05"))
 	fmt.Printf("运行路径：%s\n", AppPath)
