94fa31c099cde73a65bcf3ea79f27e49950c4398
mrzhangs520 <mrzhangs@yeah.net>
2022-09-19 14:50:40 +0800

    修复没有log文件夹启动报错的问题🤣

--- core/core.go
+++ core/core.go
@@ -6,10 +6,12 @@ import (
 	"time"
 )
 
-func Start() {
+func init() {
 	// 创建temp文件夹
 	_ = os.Mkdir(fmt.Sprintf("%s/log", AppPath), 0777)
+}
 
+func Start() {
 	fmt.Printf("当前时间：%s\n", time.Now().Format("2006-01-02 15:04:05"))
 	fmt.Printf("运行路径：%s\n", AppPath)
 	fmt.Printf("运行环境：%s\n", Mode)
